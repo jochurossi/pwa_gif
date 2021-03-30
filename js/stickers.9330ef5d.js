c4e1b8a534d887d997727072ba73674f2e34c376
jose <jose@MacBook-Pro-de-Jose.local>
2021-03-29 22:12:04 -0300

    primer commit pwa

---
+++ js/stickers.9330ef5d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["stickers"],{ab64:function(t,e,n){"use strict";n.r(e);var s=n("7a23");function a(t,e,n,a,r,c){return Object(s["q"])(),Object(s["d"])("h1",null,"Esta la vista stickers")}var r={};r.render=a;e["default"]=r}}]);
+//# sourceMappingURL=stickers.9330ef5d.js.map
---
+++ precache-manifest.6e6d24f3fd385b8b777a8b20ff9dc3d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a780a510ca22f645c0e9",
+    "url": "/css/app.d0772ad1.css"
+  },
+  {
+    "revision": "e08bead15165c3c647ca415f87b812a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0c5ca4d090d601f095e",
+    "url": "/js/about.3fc2e6af.js"
+  },
+  {
+    "revision": "a780a510ca22f645c0e9",
+    "url": "/js/app.642c2404.js"
+  },
+  {
+    "revision": "86ba998d68a8fc525e22",
+    "url": "/js/chunk-vendors.1d1c0f19.js"
+  },
+  {
+    "revision": "ab3a15af5c6df1a94ae7",
+    "url": "/js/gifs.be98552b.js"
+  },
+  {
+    "revision": "b85d32a978272bb081cc",
+    "url": "/js/stickers.9330ef5d.js"
+  },
+  {
+    "revision": "edb846ee1852bb9cc4cc3094bb5a24ec",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
